8ec7474354e7769db65a0dff9bdf42ab1c762c62
Mustafizur Rahman <mustafizur@bs-23.net>
2020-07-04 16:54:38 +0600

    Create profile Controller, Appuser, Services, upload service

---
+++ Forums.Data/Interface/IApplicationUser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Forums.Data.Models;
+
+namespace Forums.Data.Interface
+{
+    public interface IApplicationUser
+    {
+        ApplicationUser GetById(string id);
+        IEnumerable<ApplicationUser> GetAll();
+
+        Task IncrementRating(string id);
+        Task Add(ApplicationUser user);
+        Task Deactivate(ApplicationUser user);
+        Task SetProfileImage(string id, Uri uri);
+        Task BumpRating(string userId, Type type);
+    }
+}
---
+++ ForumsApp/Controllers/ProfileController.cs
@@ -0,0 +1,27 @@
+﻿using Forums.Data.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ForumsApp.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IApplicationUser _userService;
+        private readonly IUpload _uploadService;
+
+        public ProfileController(UserManager<ApplicationUser> userManager,
+            IApplicationUser userService,
+            IUpload uploadService)
+        {
+            _userManager = userManager;
+            _userService = userService;
+            _uploadService = uploadService;
+        }
+
+        public IActionResult Detail(string id)
+        {
+            return View();
+        }
+    }
+}
